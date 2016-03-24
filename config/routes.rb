8a16e1cf61e2a97c15c4b21d941278e9cffbfefb
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-24 20:23:54 +0530

    Workout controller and Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'works#index'
+  resources :works
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
