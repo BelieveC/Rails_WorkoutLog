1cc732480cba8387f5435dce472b0901c32ad8a9
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-24 23:56:43 +0530

    Add Exercise model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324144907) do
+ActiveRecord::Schema.define(version: 20160324182540) do
+
+  create_table "exercises", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "sets"
+    t.integer  "reps"
+    t.integer  "workout_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "exercises", ["workout_id"], name: "index_exercises_on_workout_id"
 
   create_table "works", force: :cascade do |t|
     t.string   "mood"
